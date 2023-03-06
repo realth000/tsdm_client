77e124b8a5be5b3e2924a7606e058fb41628e0a2
realth000 <a598919335@gmail.com>
2023-03-06 22:44:45 +0800

    feat(widgets): Use GridView in ThreadCard
    Use GridView in ThreadCard.

--- lib/widgets/thread_card.dart
+++ lib/widgets/thread_card.dart
@@ -18,66 +18,79 @@ class ThreadCard extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) => Card(
         child: Padding(
           padding: const EdgeInsets.only(left: 5, top: 2, right: 5, bottom: 2),
-          child: Row(
+          child: Column(
             children: [
-              SizedBox(
-                width: 80,
-                height: 60,
-                child: Chip(
-                  label: Text(thread.threadType!.name),
-                  backgroundColor: Colors.transparent,
-                ),
-              ),
-              smallSpacing,
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
+              Row(
+                children: [
+                  Chip(
+                    label: Text(thread.threadType!.name),
+                    backgroundColor: Colors.transparent,
+                  ),
+                  smallSpacing,
+                  Expanded(
+                    child: Text(
                       thread.title,
                       style: headerTextStyle(context),
                     ),
-                    verySmallSpacing,
-                    Row(
-                      children: [
-                        Chip(
-                          avatar: const Icon(Icons.perm_identity),
-                          label: Text('作者：${thread.author.name}'),
-                        ),
-                        smallSpacing,
-                        const Icon(Icons.access_time),
-                        Text(
-                          '发布时间：${DateFormat('yyyy-MM-dd').format(thread.publishDate)}',
-                        ),
-                      ],
-                    ),
-                    verySmallSpacing,
-                    Row(
-                      children: [
-                        Chip(
-                          avatar: const Icon(Icons.forum),
-                          label: Text('回复：${thread.replyCount}'),
-                        ),
-                        smallSpacing,
-                        const Icon(Icons.signal_cellular_alt),
-                        Text('查看：${thread.viewCount}'),
-                      ],
-                    ),
-                    verySmallSpacing,
-                    Row(
-                      children: [
-                        Chip(
-                          avatar: const Icon(Icons.record_voice_over),
-                          label: Text('最后回复：${thread.latestReplyAuthor.name}'),
-                        ),
-                        smallSpacing,
-                        const Icon(Icons.access_time),
-                        Text(
-                            '回复时间：${DateFormat('yyyy-MM-dd hh:mm:ss').format(thread.latestReplyTime)}'),
-                      ],
-                    ),
-                  ],
+                  ),
+                ],
+              ),
+              GridView(
+                shrinkWrap: true,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  mainAxisExtent: 30,
                 ),
+                children: [
+                  TextButton.icon(
+                    icon: const Icon(Icons.perm_identity),
+                    label: Text('作者：${thread.author.name}'),
+                    style: const ButtonStyle(
+                      alignment: Alignment.centerLeft,
+                    ),
+                    onPressed: () {},
+                  ),
+                  Row(
+                    children: [
+                      const Icon(Icons.access_time),
+                      smallSpacing,
+                      Text(
+                        '发布时间：${DateFormat('yyyy-MM-dd').format(thread.publishDate)}',
+                      ),
+                    ],
+                  ),
+                  TextButton.icon(
+                    icon: const Icon(Icons.forum),
+                    label: Text('回复：${thread.replyCount}'),
+                    style: const ButtonStyle(
+                      alignment: Alignment.centerLeft,
+                    ),
+                    onPressed: () {},
+                  ),
+                  Row(
+                    children: [
+                      const Icon(Icons.signal_cellular_alt),
+                      smallSpacing,
+                      Text('查看：${thread.viewCount}'),
+                    ],
+                  ),
+                  TextButton.icon(
+                    icon: const Icon(Icons.record_voice_over),
+                    label: Text('最后回复：${thread.latestReplyAuthor.name}'),
+                    style: const ButtonStyle(
+                      alignment: Alignment.centerLeft,
+                    ),
+                    onPressed: () {},
+                  ),
+                  Row(
+                    children: [
+                      const Icon(Icons.access_time),
+                      smallSpacing,
+                      Text(
+                          '回复时间：${DateFormat('yyyy-MM-dd hh:mm:ss').format(thread.latestReplyTime)}'),
+                    ],
+                  ),
+                ],
               ),
             ],
           ),
