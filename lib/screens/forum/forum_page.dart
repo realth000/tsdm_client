c6127e616e805b1846357f5e52bd2fb7cf8d8b56
realth000 <a598919335@gmail.com>
2023-09-19 02:28:00 +0800

    feat(forum): Check access permission

--- lib/screens/forum/forum_page.dart
+++ lib/screens/forum/forum_page.dart
@@ -3,6 +3,7 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:html/dom.dart' as dom;
 import 'package:tsdm_client/models/normal_thread.dart';
 import 'package:tsdm_client/states/consumer_window_state.dart';
+import 'package:tsdm_client/utils/debug.dart';
 import 'package:tsdm_client/widgets/network_list.dart';
 import 'package:tsdm_client/widgets/thread_card.dart';
 
@@ -26,16 +27,26 @@ class _ForumPageState extends ConsumerWindowState<ForumPage> {
         widget._fetchUrl,
         listBuilder: (document) {
           final normalThreadData = <NormalThread>[];
-          document
-              .getElementsByClassName('tsdm_normalthread')
-              .forEach((threadElement) {
+          final threadList =
+              document.getElementsByClassName('tsdm_normalthread');
+          if (threadList.isEmpty) {
+            final docTitle = document.getElementsByTagName('title');
+            final docMessage = document.getElementById('messagetext');
+            final docAccessRequire = docMessage?.nextElementSibling?.innerHtml;
+            final docLogin = document.getElementById('messagelogin');
+            debug(
+                'failed to build forum page, thread is empty. Maybe need to login ${docTitle.first.text} ${docMessage?.text} ${docAccessRequire ?? ''} ${docLogin == null}');
+            return normalThreadData;
+          }
+
+          for (final threadElement in threadList) {
             final thread =
                 buildNormalThreadFromElement(threadElement as dom.Element);
             if (thread == null) {
-              return;
+              continue;
             }
             normalThreadData.add(thread);
-          });
+          }
           return normalThreadData;
         },
         widgetBuilder: (context, thread) => ThreadCard(thread),
