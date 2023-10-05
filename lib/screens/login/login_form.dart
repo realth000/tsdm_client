74b43ffe6ba11dd0cb9e9082716db177b8fcfe8c
realth000 <a598919335@gmail.com>
2023-10-06 01:28:07 +0800

    feat(screens): Login screen handle login failure
    Login screen handle login failure:
    * When login failed, show dialog, or print error message if in debug mode.

--- lib/screens/login/login_form.dart
+++ lib/screens/login/login_form.dart
@@ -5,6 +5,8 @@ import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:html/dom.dart' as dom;
+import 'package:html/parser.dart' as html_parser;
 import 'package:tsdm_client/providers/net_client_provider.dart';
 import 'package:tsdm_client/screens/login/captcha_image.dart';
 import 'package:tsdm_client/utils/debug.dart';
@@ -14,15 +16,19 @@ class LoginForm extends ConsumerStatefulWidget {
     required this.redirectPath,
     required this.redirectPathParameters,
     required this.redirectExtra,
+    required this.loginHash,
+    required this.formHash,
     super.key,
   });
-
-  static const String _fakeFormUrl =
-      'https://tsdm39.com/member.php?mod=logging&action=login&infloat=yes&frommessage&inajax=1&ajaxtarget=messagelogin';
 
   final String redirectPath;
   final Map<String, String> redirectPathParameters;
   final Object? redirectExtra;
+
+  // Data needed when posting login request.
+  // Need to store in widget not state to prevent redundant web request.
+  final String loginHash;
+  final String formHash;
 
   @override
   ConsumerState<LoginForm> createState() => _LoginFormState();
@@ -34,42 +40,87 @@ class _LoginFormState extends ConsumerState<LoginForm> {
   final passwordController = TextEditingController();
   final verifyCodeController = TextEditingController();
 
-  String? _loginHash;
-  String? _formHash;
+  bool _showPassword = false;
 
-  Future<String> _fetchLoginHash() async {
-    // 返回的data是xml：
-    //
-    // <?xml version="1.0" encoding="utf-8"?>
-    // <root><![CDATA[
-    // <div id="main_messaqge_L5hJN">
-    // <div id="layer_login_L5hJN">
-    //
-    // 其中"main_message_"后面的是本次登录的loginHash，登录时需要加到url上
-    final rawData =
-        await ref.read(netClientProvider).get(LoginForm._fakeFormUrl);
-    final data = rawData.data as String;
-    final re = RegExp(r'layer_login_(?<Hash>\w+)');
-    final match = re.firstMatch(data);
-    _loginHash = match?.namedGroup('Hash');
-    if (_loginHash == null) {
-      debug('failed to get login hash');
-      return Future.error('prepare failed');
+  Future<void> _login() async {
+    if (formKey.currentState == null || !(formKey.currentState!).validate()) {
+      return;
     }
 
-    final re2 = RegExp(r'formhash" value="(?<FormHash>\w+)"');
-    final formHashMatch = re2.firstMatch(data);
-    _formHash = formHashMatch?.namedGroup('FormHash');
-    if (_formHash == null) {
-      debug('failed to get form hash');
-      return Future.error('prepare failed');
+    // login
+    final body = {
+      'formhash': widget.formHash,
+      'referer': 'https://tsdm39.com/forum.php',
+      'loginfield': 'username',
+      'username': usernameController.text,
+      'password': passwordController.text,
+      'tsdm_verify': verifyCodeController.text,
+      'questionid': 0,
+      'answer': 0,
+      'cookietime': 2592000,
+      'loginsubmit': true
+    };
+    final target =
+        'https://tsdm39.com/member.php?mod=logging&action=login&loginsubmit=yes&frommessage&loginhash=${widget.loginHash}';
+    final resp = await ref.read(netClientProvider).post(
+          target,
+          data: body,
+          options: Options(
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+          ),
+        );
+
+    // err_login_captcha_invalid
+
+    if (resp.statusCode != HttpStatus.ok) {
+      final message = 'failed to login: StatusCode=${resp.statusCode}';
+      debug(message);
+      if (mounted) {
+        return showLoginFailedDialog(context, message);
+      } else {
+        await Fluttertoast.showToast(msg: message);
+        return;
+      }
     }
 
-    debug('get login hash $_loginHash');
-    return '';
+    final document = html_parser.parse(resp.data);
+    final loginResultMessageNode = document.getElementById('messagetext');
+    if (loginResultMessageNode == null) {
+      // Impossible.
+      const message = 'failed to login: login result message node not found';
+      debug(message);
+      if (mounted) {
+        return showLoginFailedDialog(context, message);
+      } else {
+        await Fluttertoast.showToast(msg: message);
+        return;
+      }
+    }
+
+    if (mounted) {
+      // Check login result.
+      final loginResult =
+          LoginAttemptResult.fromLoginMessageNode(loginResultMessageNode);
+      switch (loginResult) {
+        case LoginAttemptResult.success:
+          debug(
+              'login success, redirect back to: path=${widget.redirectPath} with parameters=${widget.redirectPathParameters}, extra=${widget.redirectExtra}');
+          context.pushReplacementNamed(
+            widget.redirectPath,
+            pathParameters: widget.redirectPathParameters,
+            extra: widget.redirectExtra,
+          );
+        default:
+          return showLoginFailedDialog(
+            context,
+            '$loginResult: ${loginResultMessageNode.querySelector('div#messagetext > p')?.nodes.firstOrNull?.text}',
+          );
+      }
+    }
   }
 
-  Widget _buildLogin() {
+  @override
+  Widget build(BuildContext context) {
     return Form(
       key: formKey,
       child: Column(
@@ -78,39 +129,46 @@ class _LoginFormState extends ConsumerState<LoginForm> {
             'Login',
             style: Theme.of(context).textTheme.titleLarge,
           ),
+          const SizedBox(width: 10, height: 10),
           TextFormField(
             autofocus: true,
             controller: usernameController,
             decoration: const InputDecoration(
-              icon: Icon(Icons.person),
+              prefixIcon: Icon(Icons.person),
               labelText: 'Username',
             ),
             validator: (v) =>
                 v!.trim().isNotEmpty ? null : 'Username should not be empty',
           ),
+          const SizedBox(width: 10, height: 10),
           TextFormField(
             controller: passwordController,
-            decoration: const InputDecoration(
-              icon: Icon(Icons.password),
+            decoration: InputDecoration(
+              prefixIcon: const Icon(Icons.password),
               labelText: 'Password',
+              suffixIcon: IconButton(
+                icon: _showPassword
+                    ? const Icon(Icons.visibility)
+                    : const Icon(Icons.visibility_off),
+                onPressed: () {
+                  setState(() {
+                    _showPassword = !_showPassword;
+                  });
+                },
+              ),
             ),
             obscureText: true,
             validator: (v) =>
                 v!.trim().isNotEmpty ? null : 'Password should not be empty',
           ),
-          const Row(
-            children: [
-              Icon(Icons.done),
-              Text('Prepare login success'),
-            ],
-          ),
+          const SizedBox(width: 10, height: 10),
           Row(
             children: [
               Expanded(
                 child: TextFormField(
                   controller: verifyCodeController,
                   decoration: const InputDecoration(
-                    icon: Icon(Icons.pin),
+                    prefixIcon: Icon(Icons.pin),
                     labelText: 'Verify Code',
                   ),
                   validator: (v) => v!.trim().isNotEmpty
@@ -118,6 +176,7 @@ class _LoginFormState extends ConsumerState<LoginForm> {
                       : 'Verify code should not be empty',
                 ),
               ),
+              const SizedBox(width: 10, height: 10),
               ConstrainedBox(
                 constraints: const BoxConstraints(
                   maxWidth: 150,
@@ -126,65 +185,12 @@ class _LoginFormState extends ConsumerState<LoginForm> {
               ),
             ],
           ),
+          const SizedBox(width: 10, height: 10),
           Row(
             children: [
               Expanded(
                 child: ElevatedButton(
-                  onPressed: () async {
-                    if (formKey.currentState == null ||
-                        !(formKey.currentState!).validate()) {
-                      return;
-                    }
-
-                    if (_loginHash == null || _loginHash == null) {
-                      return;
-                    }
-
-                    // login
-                    final body = {
-                      'formhash': _formHash,
-                      'referer': 'https://tsdm39.com/forum.php',
-                      'loginfield': 'username',
-                      'username': usernameController.text,
-                      'password': passwordController.text,
-                      'tsdm_verify': verifyCodeController.text,
-                      'questionid': 0,
-                      'answer': 0,
-                      'cookietime': 2592000,
-                      'loginsubmit': true
-                    };
-                    final target =
-                        'https://tsdm39.com/member.php?mod=logging&action=login&loginsubmit=yes&frommessage&loginhash=${_loginHash!}';
-                    final resp = await ref.read(netClientProvider).post(
-                          target,
-                          data: body,
-                          options: Options(
-                            headers: {
-                              'Content-Type':
-                                  'application/x-www-form-urlencoded'
-                            },
-                          ),
-                        );
-
-                    if (resp.statusCode != HttpStatus.ok) {
-                      debug('failed to login: StatusCode=${resp.statusCode}');
-                      await Fluttertoast.showToast(
-                          msg:
-                              'failed to login: StatusCode=${resp.statusCode}');
-                      return;
-                    }
-
-                    if (mounted) {
-                      // FIXME: Fix redirect back and cookie storage.
-                      debug(
-                          'login success, redirect back to: path=${widget.redirectPath} with paraters=${widget.redirectPathParameters}, extra=${widget.redirectExtra}');
-                      context.pushReplacementNamed(
-                        widget.redirectPath,
-                        pathParameters: widget.redirectPathParameters,
-                        extra: widget.redirectExtra,
-                      );
-                    }
-                  },
+                  onPressed: _login,
                   child: const Text('Login'),
                 ),
               ),
@@ -194,28 +200,126 @@ class _LoginFormState extends ConsumerState<LoginForm> {
       ),
     );
   }
+}
+
+/// Enum to represent whether a login attempt succeed.
+enum LoginAttemptResult {
+  /// Login success.
+  success,
+
+  /// Captcha is not correct.
+  incorrectCaptcha,
+
+  /// Maybe a login failed.
+  ///
+  /// When showing error messages or logging, record the original message.
+  maybeInvalidUsernameOrPassword,
+
+  /// Too many login attempt and failure.
+  loginAttemptLimit,
+
+  /// Other unrecognized error received from server.
+  otherError,
+
+  /// Unknown result.
+  ///
+  /// Treat as login failed.
+  unknown;
+
+  factory LoginAttemptResult.fromLoginMessageNode(dom.Element messageNode) {
+    final message = messageNode
+        .querySelector('div#messagetext > p')
+        ?.nodes
+        .firstOrNull
+        ?.text;
+    if (message == null) {
+      debug(
+          'failed to check login result: login result message text not found');
+      return LoginAttemptResult.unknown;
+    }
+
+    // Check message result node classes.
+    // alert_right => login success.
+    // alert_info  => login failed, maybe incorrect captcha.
+    // alert_error => login failed, maybe invalid username or password.
+    final messageClasses = messageNode.classes;
+
+    if (messageClasses.contains('alert_right')) {
+      if (message.contains('欢迎您回来')) {
+        return LoginAttemptResult.success;
+      }
+
+      // Impossible unless server response page updated and changed these messages.
+      debug('login result check passed but message check maybe outdated');
+      return LoginAttemptResult.success;
+    }
+
+    if (messageClasses.contains('alert_info')) {
+      if (message.contains('err_login_captcha_invalid')) {
+        return LoginAttemptResult.incorrectCaptcha;
+      }
+
+      // Other unrecognized error.
+      debug(
+          'login result check not passed: alert_info class with unknown message: $message');
+      return LoginAttemptResult.otherError;
+    }
+
+    if (messageClasses.contains('alert_error')) {
+      if (message.contains('登录失败')) {
+        return LoginAttemptResult.maybeInvalidUsernameOrPassword;
+      }
+
+      if (message.contains('密码错误次数过多')) {
+        return LoginAttemptResult.loginAttemptLimit;
+      }
+
+      // Other unrecognized error.
+      debug(
+          'login result check not passed: alert_error with unknown message: $message');
+      return LoginAttemptResult.otherError;
+    }
+
+    debug('login result check not passed: unknown result');
+    return LoginAttemptResult.unknown;
+  }
 
   @override
-  Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: _fetchLoginHash(),
-      builder: (context, snapshot) {
-        if (snapshot.hasError) {
-          return Text('failed to get login hash :${snapshot.error}');
-        }
-
-        if (snapshot.hasData) {
-          return _buildLogin();
-        }
-
-        return const Row(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            CircularProgressIndicator(),
-            Text('Preparing login'),
-          ],
-        );
-      },
-    );
+  String toString() {
+    switch (this) {
+      case LoginAttemptResult.success:
+        return 'login success';
+      case LoginAttemptResult.incorrectCaptcha:
+        return 'incorrect captcha';
+      case LoginAttemptResult.maybeInvalidUsernameOrPassword:
+        return 'maybe invalid username or password';
+      case LoginAttemptResult.loginAttemptLimit:
+        return 'too many login attempts';
+      case LoginAttemptResult.otherError:
+        return 'other errors';
+      case LoginAttemptResult.unknown:
+        return 'failed with unknown reason';
+    }
   }
+}
+
+Future<void> showLoginFailedDialog(BuildContext context, String message) async {
+  return showDialog(
+    context: context,
+    builder: (context) {
+      return AlertDialog(
+        scrollable: true,
+        title: const Text('Login failed'),
+        content: Text(message),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: const Text('Ok'),
+          )
+        ],
+      );
+    },
+  );
 }
--- lib/screens/login/login_page.dart
+++ lib/screens/login/login_page.dart
@@ -1,27 +1,96 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:tsdm_client/providers/net_client_provider.dart';
 import 'package:tsdm_client/screens/login/login_form.dart';
+import 'package:tsdm_client/utils/debug.dart';
 
-class LoginPage extends ConsumerWidget {
+class LoginPage extends ConsumerStatefulWidget {
   const LoginPage({required this.redirectBackState, super.key});
 
   final GoRouterState redirectBackState;
 
+  static const String _fakeFormUrl =
+      'https://tsdm39.com/member.php?mod=logging&action=login&infloat=yes&frommessage&inajax=1&ajaxtarget=messagelogin';
+
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    return Center(
-      child: ConstrainedBox(
-        constraints: const BoxConstraints(
-          maxHeight: 500,
-          maxWidth: 500,
-        ),
-        child: LoginForm(
-          redirectPath: redirectBackState.fullPath!,
-          redirectPathParameters: redirectBackState.pathParameters,
-          redirectExtra: redirectBackState.extra,
-        ),
-      ),
+  ConsumerState<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends ConsumerState<LoginPage> {
+  String? loginHash;
+  String? formHash;
+
+  Future<String> _fetchLoginHash() async {
+    // 返回的data是xml：
+    //
+    // <?xml version="1.0" encoding="utf-8"?>
+    // <root><![CDATA[
+    // <div id="main_messaqge_L5hJN">
+    // <div id="layer_login_L5hJN">
+    //
+    // 其中"main_message_"后面的是本次登录的loginHash，登录时需要加到url上
+    final rawData =
+        await ref.read(netClientProvider).get(LoginPage._fakeFormUrl);
+    final data = rawData.data as String;
+    final re = RegExp(r'layer_login_(?<Hash>\w+)');
+    final match = re.firstMatch(data);
+    loginHash = match?.namedGroup('Hash');
+    if (loginHash == null) {
+      debug('failed to get login hash');
+      return Future.error('prepare failed');
+    }
+
+    final re2 = RegExp(r'formhash" value="(?<FormHash>\w+)"');
+    final formHashMatch = re2.firstMatch(data);
+    formHash = formHashMatch?.namedGroup('FormHash');
+    if (formHash == null) {
+      debug('failed to get form hash');
+      return Future.error('prepare failed');
+    }
+
+    debug('get login hash $loginHash');
+    return '';
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: _fetchLoginHash(),
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return Text('failed to get login hash: ${snapshot.error}');
+        }
+
+        if (snapshot.hasData) {
+          return Center(
+            child: ConstrainedBox(
+              constraints: const BoxConstraints(
+                maxHeight: 500,
+                maxWidth: 500,
+              ),
+              child: LoginForm(
+                redirectPath: widget.redirectBackState.fullPath!,
+                redirectPathParameters: widget.redirectBackState.pathParameters,
+                redirectExtra: widget.redirectBackState.extra,
+                loginHash: loginHash!,
+                formHash: formHash!,
+              ),
+            ),
+          );
+        }
+
+        return const Center(
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              CircularProgressIndicator(),
+              SizedBox(width: 10, height: 10),
+              Text('Preparing login'),
+            ],
+          ),
+        );
+      },
     );
   }
 }
