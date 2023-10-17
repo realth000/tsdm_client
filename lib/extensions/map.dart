d831570d83a9c0fc418ae333d17d67e800e34639
realth000 <a598919335@gmail.com>
2023-10-18 02:54:06 +0800

    feat(extensions): Add archiver route parse extensions

---
+++ lib/extensions/map.dart
@@ -0,0 +1,21 @@
+import 'package:tsdm_client/routes/screen_paths.dart';
+
+extension ParseRoute on Map<String, String> {
+  /// Parse to archiver url.
+  ///
+  /// $host/archiver/?fid=$fid;
+  /// $host/archiver/?fid=$tid;
+  String? parseArchiverUrl(String screenPath) {
+    final route = switch (screenPath) {
+      ScreenPaths.forum => this['fid'] == null ? null : "fid=${this['fid']!}",
+      ScreenPaths.thread => this['tid'] == null ? null : "tid=${this['tid']!}",
+      _ => null,
+    };
+
+    if (route == null) {
+      return null;
+    }
+
+    return 'https://tsdm39.com/archiver?$route';
+  }
+}
