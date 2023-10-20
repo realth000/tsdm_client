e8230f62372062d12315dd03d47354e24ab136b5
realth000 <a598919335@gmail.com>
2023-10-21 07:07:22 +0800

    feat(widgets): Update ThreadCard layout

--- lib/widgets/thread_card.dart
+++ lib/widgets/thread_card.dart
@@ -6,7 +6,6 @@ import 'package:tsdm_client/models/normal_thread.dart';
 import 'package:tsdm_client/routes/screen_paths.dart';
 import 'package:tsdm_client/themes/widget_themes.dart';
 import 'package:tsdm_client/utils/time.dart';
-import 'package:tsdm_client/widgets/space.dart';
 
 /// Card to show thread info.
 class ThreadCard extends ConsumerWidget {
@@ -20,147 +19,69 @@ class ThreadCard extends ConsumerWidget {
   final _currentTime = DateTime.now();
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) => Card(
-        child: Padding(
-          padding: const EdgeInsets.only(left: 5, top: 2, right: 5, bottom: 2),
-          child: Column(
-            children: [
-              Row(
-                children: [
-                  Chip(
-                    label: Text(thread.threadType!.name),
-                    backgroundColor: Colors.transparent,
-                  ),
-                  smallSpacing,
-                  Expanded(
-                    child: TextButton(
-                      onPressed: () {
-                        context.pushNamed(
-                          ScreenPaths.thread,
-                          pathParameters: <String, String>{
-                            'tid': thread.threadID,
-                          },
-                          extra: <String, dynamic>{
-                            'appBarTitle': thread.title,
-                          },
-                        );
-                      },
-                      child: Align(
-                        alignment: Alignment.centerLeft,
-                        child: Text(
-                          thread.title,
-                          style: headerTextStyle(context),
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              GridView(
-                physics: const NeverScrollableScrollPhysics(),
-                shrinkWrap: true,
-                gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-                  maxCrossAxisExtent: 170,
-                  mainAxisExtent: 35,
-                ),
-                children: [
-                  TextButton.icon(
-                    icon: const Icon(
-                      Icons.perm_identity,
-                      size: smallIconSize,
-                    ),
-                    label: Text(
-                      thread.author.name,
-                      style: const TextStyle(fontSize: smallTextSize),
-                    ),
-                    style: const ButtonStyle(
-                      alignment: Alignment.centerLeft,
-                    ),
-                    onPressed: () {},
-                  ),
-                  Tooltip(
-                    message:
-                        DateFormat('yyyy-MM-dd').format(thread.publishDate),
-                    child: TextButton.icon(
-                      icon: const Icon(
-                        Icons.access_time,
-                        size: smallIconSize,
-                      ),
-                      label: Text(
-                        DateFormat('yyyy-MM-dd').format(thread.publishDate),
-                        style: const TextStyle(fontSize: smallTextSize),
-                      ),
-                      style: const ButtonStyle(
-                        alignment: Alignment.centerLeft,
-                      ),
-                      onPressed: () {},
-                    ),
-                  ),
-                  TextButton.icon(
-                    icon: const Icon(
-                      Icons.forum,
-                      size: smallIconSize,
-                    ),
-                    label: Text(
-                      '${thread.replyCount}',
-                      style: const TextStyle(fontSize: smallTextSize),
-                    ),
-                    style: const ButtonStyle(
-                      alignment: Alignment.centerLeft,
-                    ),
-                    onPressed: () {},
-                  ),
-                  TextButton.icon(
-                    icon: const Icon(
-                      Icons.signal_cellular_alt,
-                      size: smallIconSize,
-                    ),
-                    label: Text(
-                      '${thread.viewCount}',
-                      style: const TextStyle(fontSize: smallTextSize),
-                    ),
-                    style: const ButtonStyle(
-                      alignment: Alignment.centerLeft,
-                    ),
-                    onPressed: () {},
-                  ),
-                  TextButton.icon(
-                    icon: const Icon(
-                      Icons.record_voice_over,
-                      size: smallIconSize,
-                    ),
-                    label: Text(
-                      thread.latestReplyAuthor.name,
-                      style: const TextStyle(fontSize: smallTextSize),
-                    ),
-                    style: const ButtonStyle(
-                      alignment: Alignment.centerLeft,
-                    ),
-                    onPressed: () {},
-                  ),
-                  Tooltip(
-                    message: DateFormat('yyyy-MM-dd hh:mm:ss')
-                        .format(thread.latestReplyTime),
-                    child: TextButton.icon(
-                      icon: const Icon(
-                        Icons.hourglass_bottom,
-                        size: smallIconSize,
-                      ),
-                      label: Text(
-                        timeDifferenceToString(
-                            _currentTime, thread.latestReplyTime),
-                        style: const TextStyle(fontSize: smallTextSize),
-                      ),
-                      style: const ButtonStyle(
-                        alignment: Alignment.centerLeft,
-                      ),
-                      onPressed: () {},
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
+  Widget build(BuildContext context, WidgetRef ref) {
+    final infoList = [
+      (Icons.forum, '${thread.replyCount}'),
+      (Icons.signal_cellular_alt, '${thread.viewCount}'),
+      (Icons.record_voice_over, thread.latestReplyAuthor.name),
+      (
+        Icons.hourglass_bottom,
+        timeDifferenceToString(_currentTime, thread.latestReplyTime),
+      ),
+    ];
+
+    return Card(
+      child: ListTile(
+        leading: Chip(
+          label: Text(thread.threadType!.name),
+          backgroundColor: Colors.transparent,
         ),
-      );
+        title: Text(thread.title),
+        subtitle: Row(
+          children: infoList
+              .map(
+                (e) => Expanded(
+                    child: Row(
+                  children: [
+                    Icon(e.$1, size: smallIconSize),
+                    const SizedBox(width: 5),
+
+                    /// Wrap in expand to make sure `overflow` in text works.
+                    Expanded(
+                      child: Text(
+                        e.$2,
+                        style: const TextStyle(fontSize: smallTextSize),
+                        maxLines: 1,
+                        overflow: TextOverflow.clip,
+                      ),
+                    ),
+                  ],
+                )),
+              )
+              .toList(),
+        ),
+        trailing: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              thread.author.name,
+              style: Theme.of(context).textTheme.bodyMedium,
+            ),
+            Text(DateFormat('yyyy-MM-dd').format(thread.publishDate)),
+          ],
+        ),
+        onTap: () {
+          context.pushNamed(
+            ScreenPaths.thread,
+            pathParameters: <String, String>{
+              'tid': thread.threadID,
+            },
+            extra: <String, dynamic>{
+              'appBarTitle': thread.title,
+            },
+          );
+        },
+      ),
+    );
+  }
 }
