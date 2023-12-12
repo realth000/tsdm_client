83ec784712ac62ac124febbb4566c7a33b1de19f
realth000 <a598919335@gmail.com>
2023-12-12 11:19:51 +0800

    feat(widgets): Use filled selected icon in app nav bar

--- lib/widgets/app_navitaion_bar.dart
+++ lib/widgets/app_navitaion_bar.dart
@@ -8,11 +8,13 @@ import 'package:tsdm_client/routes/screen_paths.dart';
 class _NavigationBarItem {
   _NavigationBarItem({
     required this.icon,
+    required this.selectedIcon,
     required this.label,
     required this.targetPath,
   });
 
   final Icon icon;
+  final Icon selectedIcon;
   final String label;
   final String targetPath;
 }
@@ -30,21 +32,25 @@ class _AppNavigationBarState extends ConsumerState<AppNavigationBar> {
     final barItems = [
       _NavigationBarItem(
         icon: const Icon(Icons.home_outlined),
+        selectedIcon: const Icon(Icons.home),
         label: context.t.navigation.homepage,
         targetPath: ScreenPaths.homepage,
       ),
       _NavigationBarItem(
         icon: const Icon(Icons.topic_outlined),
+        selectedIcon: const Icon(Icons.topic),
         label: context.t.navigation.topics,
         targetPath: ScreenPaths.topic,
       ),
       _NavigationBarItem(
         icon: const Icon(Icons.person_outline),
+        selectedIcon: const Icon(Icons.person),
         label: context.t.navigation.profile,
         targetPath: ScreenPaths.profile,
       ),
       _NavigationBarItem(
         icon: const Icon(Icons.settings_outlined),
+        selectedIcon: const Icon(Icons.settings),
         label: context.t.navigation.settings,
         targetPath: ScreenPaths.settings,
       ),
@@ -52,7 +58,8 @@ class _AppNavigationBarState extends ConsumerState<AppNavigationBar> {
 
     return NavigationBar(
       destinations: barItems
-          .map((e) => NavigationDestination(icon: e.icon, label: e.label))
+          .map((e) => NavigationDestination(
+              icon: e.icon, selectedIcon: e.selectedIcon, label: e.label))
           .toList(),
       selectedIndex: ref.watch(appNavigationBarIndexProvider),
       onDestinationSelected: (index) {
