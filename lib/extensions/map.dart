b7c146d329da3530ae152ac9127c7b475ee216e6
realth000 <a598919335@gmail.com>
2023-12-17 02:49:12 +0800

    fix(ext): Fix exception when redirect to pinned thread

--- lib/extensions/map.dart
+++ lib/extensions/map.dart
@@ -24,7 +24,7 @@ extension ParseRoute on Map<String, String> {
 extension ModifyMap<K, V> on Map<K, V> {
   /// Return a new map that copies current map and [other] together.
   Map<K, V> copyWith(Map<K, V> other) {
-    final ret = this;
+    final ret = Map<K, V>.from(this);
     for (final i in other.entries) {
       ret[i.key] = i.value;
     }
