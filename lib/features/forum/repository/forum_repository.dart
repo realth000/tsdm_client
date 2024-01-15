62c6eda8ada82fa839b28c8e99e33647c811078d
realth000 <a598919335@gmail.com>
2024-01-16 02:30:25 +0800

    fix(repo): Fix forum repo not updates recorded server time

--- lib/features/forum/repository/forum_repository.dart
+++ lib/features/forum/repository/forum_repository.dart
@@ -4,6 +4,7 @@ import 'package:tsdm_client/constants/url.dart';
 import 'package:tsdm_client/exceptions/exceptions.dart';
 import 'package:tsdm_client/instance.dart';
 import 'package:tsdm_client/shared/providers/net_client_provider/net_client_provider.dart';
+import 'package:tsdm_client/shared/providers/server_time_provider/sevrer_time_provider.dart';
 import 'package:universal_html/html.dart' as uh;
 import 'package:universal_html/parsing.dart';
 
@@ -26,6 +27,7 @@ class ForumRepository {
       throw HttpRequestFailedException(resp.statusCode!);
     }
     final document = parseHtmlDocument(resp.data as String);
+    getIt.get<ServerTimeProvider>().updateServerTimeWithDocument(document);
     return document;
   }
 }
