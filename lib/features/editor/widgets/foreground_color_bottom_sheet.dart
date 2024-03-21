87620310da7f9539478ce51f89fd0dcd3b49d8f0
realth000 <a598919335@gmail.com>
2024-03-22 00:00:39 +0800

    feat(editor): Roll editor and support clear font color

--- lib/features/editor/widgets/foreground_color_bottom_sheet.dart
+++ lib/features/editor/widgets/foreground_color_bottom_sheet.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bbcode_editor/flutter_bbcode_editor.dart';
 import 'package:tsdm_client/constants/layout.dart';
+import 'package:tsdm_client/generated/i18n/strings.g.dart';
 
 /// Show a bottom sheet provides all available foreground colors for user to
 /// choose.
@@ -33,28 +34,52 @@ class _ForegroundColorBottomSheetState
     return Scaffold(
       body: Padding(
         padding: edgeInsetsL15T15R15B15,
-        child: GridView.builder(
-          shrinkWrap: true,
-          gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-            maxCrossAxisExtent: 50,
-            mainAxisSpacing: 10,
-            crossAxisSpacing: 10,
-            mainAxisExtent: 50,
-          ),
-          itemCount: ForegroundColor.values.length,
-          itemBuilder: (context, index) {
-            final color = ForegroundColor.values[index].color;
-            return GestureDetector(
-              onTap: () async {
-                Navigator.of(context).pop();
-                await widget.controller.setForegroundColor(color);
-              },
-              child: CircleAvatar(
-                radius: 15,
-                backgroundColor: color,
+        child: Column(
+          children: [
+            SizedBox(
+              height: 50,
+              child: Center(
+                child: Text(context.t.bbcodeEditor.foregroundColor.title),
               ),
-            );
-          },
+            ),
+            sizedBoxW10H10,
+            GridView.builder(
+              shrinkWrap: true,
+              gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+                maxCrossAxisExtent: 50,
+                mainAxisSpacing: 10,
+                crossAxisSpacing: 10,
+                mainAxisExtent: 50,
+              ),
+              itemCount: ForegroundColor.values.length,
+              itemBuilder: (context, index) {
+                final color = ForegroundColor.values[index].color;
+                return GestureDetector(
+                  onTap: () async {
+                    Navigator.of(context).pop();
+                    await widget.controller.setForegroundColor(color);
+                  },
+                  child: CircleAvatar(
+                    radius: 15,
+                    backgroundColor: color,
+                  ),
+                );
+              },
+            ),
+            sizedBoxW5H5,
+            Row(
+              children: [
+                const Spacer(),
+                TextButton(
+                  child: Text(context.t.general.reset),
+                  onPressed: () async {
+                    Navigator.of(context).pop();
+                    await widget.controller.clearForegroundColor();
+                  },
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
