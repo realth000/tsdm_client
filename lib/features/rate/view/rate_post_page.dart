realth000/tsdm_client

commit 4d132fe719159d28c83d31953e70b273bcda83c5

fix(rate): Fix false nagetive action when hit limit error

lib/features/rate/view/rate_post_page.dart
@@ -206,18 +206,14 @@ class _RatePostPageState extends State<RatePostPage> {
         listener: (context, state) {
           if (state.status == RateStatus.failed) {
             if (state.shouldRetry == false) {
-              // Show reason and pop back if we should not retry.
-              showSnackBar(
-                context: context,
-                message:
-                    state.failedReason ?? context.t.ratePostPage.failedToRate,
-              );
               Navigator.of(context).pop();
               return;
             }
+            // Show reason and pop back if we should not retry.
             showSnackBar(
               context: context,
-              message: context.t.ratePostPage.failedToRate,
+              message:
+                  state.failedReason ?? context.t.ratePostPage.failedToRate,
             );
             context.read<RateBloc>().add(
                   RateFetchInfoRequested(

lib/features/rate/bloc/rate_bloc.dart
@@ -72,6 +72,7 @@ final class RateBloc extends Bloc<RateEvent, RateState> {
       debug('failed to rate: $e');
       emit(state.copyWith(status: RateStatus.failed));
     } on RateFailedException catch (e) {
+      debug('failed to rate: $e');
       emit(state.copyWith(status: RateStatus.failed, failedReason: e.reason));
     }
   }

lib/features/rate/repository/rate_repository.dart
@@ -27,6 +27,12 @@ final class RateRepository {
   /// Regexp to grep the error text from response html body.
   static final _errorTextRe = RegExp('alert_error">(?<error>[^<]+)<');
 
+  /// Rate limit handler function text.
+  ///
+  /// User will trigger this error when too many points rated in 24 hour.
+  static final _errorHandleRateRe =
+      RegExp(r"{errorhandle_rate\('(?<error>[^']+)',");
+
   /// Fetch rate info for given [pid].
   ///
   /// # Exception
@@ -86,6 +92,11 @@ final class RateRepository {
     final errorText = _errorTextRe.firstMatch(data)?.namedGroup('error');
     if (errorText != null) {
       throw RateFailedException(errorText);
+    }
+    final errorHandleRateText =
+        _errorHandleRateRe.firstMatch(data)?.namedGroup('error');
+    if (errorHandleRateText != null) {
+      throw RateFailedException(errorHandleRateText);
     }
   }
 }

commit 867f34b4812a19c4faa54112f3f851e887f7b5e0

fix(rate): Fix no message when rate failed
Introduced in 4d132fe

lib/features/rate/view/rate_post_page.dart
@@ -241,16 +241,16 @@ class _RatePostPageState extends State<RatePostPage> {
       child: BlocListener<RateBloc, RateState>(
         listener: (context, state) {
           if (state.status == RateStatus.failed) {
-            if (state.shouldRetry == false) {
-              Navigator.of(context).pop();
-              return;
-            }
             // Show reason and pop back if we should not retry.
             showSnackBar(
               context: context,
               message:
                   state.failedReason ?? context.t.ratePostPage.failedToRate,
             );
+            if (state.shouldRetry == false) {
+              Navigator.of(context).pop();
+              return;
+            }
             context.read<RateBloc>().add(
                   RateFetchInfoRequested(
                     pid: widget.pid,