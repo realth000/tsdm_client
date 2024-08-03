8baa1667ae15b1347ff08ec3578ca9904716f48a
realth000 <a598919335@gmail.com>
2024-08-03 21:56:39 +0800

    fix(cache): Fix incorrect image state when cache is invalid

--- lib/features/cache/repository/image_cache_repository.dart
+++ lib/features/cache/repository/image_cache_repository.dart
@@ -5,6 +5,7 @@ import 'package:rxdart/rxdart.dart';
 import 'package:tsdm_client/features/cache/models/models.dart';
 import 'package:tsdm_client/shared/providers/image_cache_provider/image_cache_provider.dart';
 import 'package:tsdm_client/shared/providers/net_client_provider/net_client_provider.dart';
+import 'package:tsdm_client/utils/debug.dart';
 
 /// Repository of global image cache.
 final class ImageCacheRepository {
@@ -42,9 +43,17 @@ final class ImageCacheRepository {
     if (!force) {
       final cacheInfo = _imageCacheProvider.getCacheInfo(url);
       if (cacheInfo != null) {
-        final imageData = await _imageCacheProvider.getCache(url);
-        _controller.add(ImageCacheSuccessResponse(url, imageData));
-        return;
+        await _imageCacheProvider.getCache(url).then((x) {
+          // Cache file may be deleted by external operations.
+          // Only reply a success response when cache is valid.
+          _controller.add(ImageCacheSuccessResponse(url, x));
+          return;
+        }).onError((e, st) {
+          // Some error in reading cache, consider cache as invalid.
+          // Do NOT return here, continue the caching progress.
+          debug('failed to update image cache: $e');
+          _controller.add(ImageCacheFailedResponse(url));
+        });
       }
     }
 
