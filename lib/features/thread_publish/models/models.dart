d43c936c04bce6fd29feb32568795aa6163dfd63
realth000 <a598919335@gmail.com>
2024-08-28 01:37:32 +0800

    fat(thread): Init publishing thread
    This commit add basic models definition for publishing new threads and
    saving thread drafts.
    
    But further consideration need to be done before finishing all code:
    
    * Now the model used in publishing new thread is similar to the one used
      in editing post. Maybe can be coupled together.
    * Do not know the exact action when posting thread and editing draft.
      The former one seems is "newthread" and latter one is "edit" but not
      sure.
    * The new thread edit page may have almost the same functionality with
      page in editing post, especially editting an 1st floor post. Maybe
      these two feature can be mixed together.
    * Some parameters will not support as they are barely used and have
      moderator permission requirements.
    * Need warning before posting a new thread, showing forum name to post.
      to and remind forum rules.
    * Main concern is should we separate publishing thread apart from post
      editing since they have main same functionality.

---
+++ lib/features/thread_publish/models/models.dart
@@ -0,0 +1,5 @@
+import 'package:dart_mappable/dart_mappable.dart';
+import 'package:tsdm_client/features/post/models/post_edit_content.dart';
+
+part 'models.mapper.dart';
+part 'thread_publish_info.dart';
---
+++ lib/features/thread_publish/models/thread_publish_info.dart
@@ -0,0 +1,81 @@
+part of 'models.dart';
+
+/// Model of info then posting thread data to server.
+///
+/// Maybe used in publishing a new thread or create/edit drafts.
+///
+/// Many of the options are shared with post editing.
+/// But the action is "newthread" with unknown tid/pid.
+@MappableClass()
+final class ThreadPublishInfo with ThreadPublishInfoMappable {
+  /// Constructor.
+  const ThreadPublishInfo({
+    required this.formHash,
+    required this.postTime,
+    required this.delAttachOp,
+    required this.wysiwyg,
+    required this.fid,
+    required this.typeId,
+    required this.checkbox,
+    required this.subject,
+    required this.message,
+    required this.price,
+    required this.readPerm,
+    required this.save,
+    required this.options,
+  });
+
+  /// Form hash used in action
+  final String formHash;
+
+  /// Timestamp when post info.
+  final String postTime;
+
+  /// Some info, always empty.
+  ///
+  /// "0"
+  ///
+  /// Seems is the operation on deleting thread attachment.
+  final String delAttachOp;
+
+  /// WYSIWYG, always zero.
+  ///
+  /// "0".
+  final String wysiwyg;
+
+  /// Forum id to post thread in.
+  ///
+  /// Fid saved in query parameter with "topicsubmit".
+  final String fid;
+
+  /// Thread type id.
+  ///
+  /// Provided by server, selected by user.
+  final String typeId;
+
+  /// Checkbox?
+  ///
+  /// Always "0"
+  final String checkbox;
+
+  /// Subject of thread.
+  final String subject;
+
+  /// Thread body.
+  final String message;
+
+  /// Sell price used in post.
+  final String price;
+
+  /// Least permission to read this thread.
+  final String readPerm;
+
+  /// Save to draft or not.
+  ///
+  /// * Draft if 0.
+  /// * Publish if 1.
+  final String save;
+
+  /// Options.
+  final List<PostEditContentOption> options;
+}
