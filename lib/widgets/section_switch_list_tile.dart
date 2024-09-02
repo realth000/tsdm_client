31da7464e32f186e1670e392f3494387f9d98e01
realth000 <a598919335@gmail.com>
2024-09-03 00:23:45 +0800

    feat(widgets): Add check icon to section list tile

--- lib/widgets/section_switch_list_tile.dart
+++ lib/widgets/section_switch_list_tile.dart
@@ -1,6 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:tsdm_client/constants/layout.dart';
 
+/// Const builder for state property when selected.
+class WidgetStatePropertySelected<T> implements WidgetStateProperty<T?> {
+  /// Constructor
+  const WidgetStatePropertySelected(this.value);
+
+  /// The value of the property that will be used for all states.
+  final T? value;
+
+  @override
+  T? resolve(Set<WidgetState> states) =>
+      states.contains(WidgetState.selected) ? value : null;
+
+  @override
+  String toString() => 'WidgetStatePropertySelected($value)';
+}
+
 /// [SwitchListTile] used in section.
 class SectionSwitchListTile extends SwitchListTile {
   /// Constructor.
@@ -18,7 +34,8 @@ class SectionSwitchListTile extends SwitchListTile {
     super.thumbColor,
     super.trackColor,
     super.trackOutlineColor,
-    super.thumbIcon,
+    super.thumbIcon =
+        const WidgetStatePropertySelected(Icon(Icons.check_outlined)),
     super.materialTapTargetSize,
     super.dragStartBehavior,
     super.mouseCursor,
