017e9d33e6cd4d4349339d197db89fe6fbccf9c7
realth000 <a598919335@gmail.com>
2025-02-22 20:39:00 +0800

    feat(thread): redirect entry on operation log item

--- lib/features/thread/v1/widgets/operation_log_card.dart
+++ lib/features/thread/v1/widgets/operation_log_card.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
 import 'package:tsdm_client/constants/layout.dart';
 import 'package:tsdm_client/extensions/date_time.dart';
 import 'package:tsdm_client/extensions/fp.dart';
 import 'package:tsdm_client/features/thread/v1/repository/thread_repository.dart';
 import 'package:tsdm_client/i18n/strings.g.dart';
+import 'package:tsdm_client/routes/screen_paths.dart';
 import 'package:tsdm_client/widgets/heroes.dart';
 import 'package:tsdm_client/widgets/single_line_text.dart';
 
@@ -34,8 +36,14 @@ Future<void> _showOperationLogDialog(BuildContext context, String tid) async {
             final content = actions.unwrap().map(
               (e) => ListTile(
                 isThreeLine: true,
-                leading: HeroUserAvatar(username: e.username, avatarUrl: null, disableHero: true),
-                title: Text(e.username),
+                leading: GestureDetector(
+                  onTap: () async => context.pushNamed(ScreenPaths.profile, queryParameters: {'username': e.username}),
+                  child: HeroUserAvatar(username: e.username, avatarUrl: null, disableHero: true),
+                ),
+                title: GestureDetector(
+                  onTap: () async => context.pushNamed(ScreenPaths.profile, queryParameters: {'username': e.username}),
+                  child: Text(e.username),
+                ),
                 subtitle: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
