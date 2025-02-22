00a6e364b326442b1fc1ec3245cfce943dbd0bd9
realth000 <a598919335@gmail.com>
2025-02-22 19:16:26 +0800

    fix(thread): remove unused import

--- lib/features/thread/v1/widgets/operation_log_card.dart
+++ lib/features/thread/v1/widgets/operation_log_card.dart
@@ -1,4 +1,3 @@
-import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:tsdm_client/constants/layout.dart';
