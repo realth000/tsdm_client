3d30d0db6043fa02db259d15a3dba751743c5deb
realth000 <a598919335@gmail.com>
2025-03-05 04:08:17 +0800

    feat(settings): use slider to control auto sync duration
    Closes #212

--- lib/features/settings/widgets/auto_sync_notice_dialog.dart
+++ lib/features/settings/widgets/auto_sync_notice_dialog.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:tsdm_client/extensions/duration.dart';
+import 'package:tsdm_client/constants/layout.dart';
 import 'package:tsdm_client/i18n/strings.g.dart';
 
 /// Dialog for user selecting a duration on auto sync notice feature.
-class AutoSyncNoticeDialog extends StatelessWidget {
+class AutoSyncNoticeDialog extends StatefulWidget {
   /// Constructor.
   const AutoSyncNoticeDialog(this.currentSeconds, {super.key});
 
@@ -12,39 +12,94 @@ class AutoSyncNoticeDialog extends StatelessWidget {
   final int currentSeconds;
 
   @override
+  State<AutoSyncNoticeDialog> createState() => _AutoSyncNoticeDialogState();
+}
+
+class _AutoSyncNoticeDialogState extends State<AutoSyncNoticeDialog> {
+  late double _choiceIndex;
+
+  static const allTimes = [
+    // 1 min
+    60,
+
+    // 2 min
+    120,
+
+    // 3 min
+    180,
+
+    // 5 min
+    300,
+
+    // 10 min
+    //
+    // The default one.
+    600,
+
+    // 20 min
+    1200,
+
+    // 30 min
+    1800,
+
+    // 40 min
+    2400,
+
+    // 1 hour
+    3600,
+
+    // Never
+    -1,
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    _choiceIndex =
+        allTimes.contains(widget.currentSeconds) ? allTimes.indexOf(widget.currentSeconds).toDouble() : 600.0;
+  }
+
+  @override
   Widget build(BuildContext context) {
     final tr = context.t.settingsPage.behaviorSection.autoSyncNotice;
+
+    final time = allTimes[_choiceIndex.toInt()];
+
+    final currentTimeText = switch (time) {
+      < 0 => context.t.general.never,
+      >= 0 && < 3600 => context.t.general.minutes(value: (time / 60).toInt()),
+      _ => context.t.general.hours(value: (time / 3600).toInt()),
+    };
+
     return AlertDialog(
       scrollable: true,
       title: Text(tr.title),
-      content: SingleChildScrollView(
-        child: Column(
-          children: [
-            ...[60, 300, 600, 1800, 3600].map(
-              (e) => RadioListTile(
-                title: Text(Duration(seconds: e).readable(context)),
-                value: e,
-                groupValue: currentSeconds,
-                onChanged: (v) {
-                  if (v != null) {
-                    context.pop(v);
-                  }
-                },
-              ),
-            ),
-            RadioListTile(
-              title: Text(context.t.general.never),
-              value: -1,
-              groupValue: currentSeconds,
-              onChanged: (v) {
-                if (v != null) {
-                  context.pop(v);
-                }
-              },
-            ),
-          ],
-        ),
+      content: Column(
+        children: [
+          Text(
+            currentTimeText,
+            style: Theme.of(context).textTheme.titleMedium?.copyWith(color: Theme.of(context).colorScheme.primary),
+          ),
+          sizedBoxW12H12,
+          Slider(
+            autofocus: true,
+            // Since flutter 3.29
+            // ignore: deprecated_member_use
+            year2023: false,
+            max: allTimes.length.toDouble() - 1,
+            value: _choiceIndex,
+            divisions: allTimes.length - 1,
+            onChanged: (v) {
+              setState(() {
+                _choiceIndex = v;
+              });
+            },
+          ),
+        ],
       ),
+      actions: [
+        TextButton(child: Text(context.t.general.ok), onPressed: () => context.pop(allTimes[_choiceIndex.toInt()])),
+      ],
     );
   }
 }
