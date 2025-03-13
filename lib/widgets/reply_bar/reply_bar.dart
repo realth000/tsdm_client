841bd31cc5963a3d996ec051b89a14c13fa15109
realth000 <a598919335@gmail.com>
2025-03-14 05:46:06 +0800

    fix(reply_bar): fix text not sync to outer text field if is empty
    This issue is introduced in c357155 where fixing a double dispose
    isssue, but it's not clear why skip the sync when text is empty.

--- lib/widgets/reply_bar/reply_bar.dart
+++ lib/widgets/reply_bar/reply_bar.dart
@@ -651,7 +651,7 @@ final class _ReplyBarState extends State<_ReplyBar> with LoggerMixin {
     _replyFocusNode.dispose();
     _authStatusSub.cancel();
     final text = _replyRichController.toBBCode();
-    if (text.trim().isNotEmpty && _canSyncBBCodeOnDispose) {
+    if (_canSyncBBCodeOnDispose) {
       // Only save text that intend to reply when that text is not empty.
       //
       // Only send text to outside controller if could do so: In some situation
