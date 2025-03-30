74a98d7cb6a0c673df5070857df6ee3595bd93a3
realth000 <a598919335@gmail.com>
2025-03-31 01:22:48 +0800

    refactor(bubble): extract bubble painter

---
+++ lib/widgets/bubble.dart
@@ -0,0 +1,156 @@
+//custom painter use to create the shape of the chat bubble
+import 'package:flutter/material.dart';
+
+/// Custom painter provides chat bubble background.
+///
+/// ref:  https://github.com/prahack/chat_bubbles/blob/master/lib/bubbles/bubble_special_three.dart
+class BubblePainter extends CustomPainter {
+  /// Constructor.
+  const BubblePainter({required this.color, required this.alignment, required this.tail});
+
+  /// Bubble color.
+  final Color color;
+
+  /// Tail alignment.
+  final Alignment alignment;
+
+  /// Enable bubble tail or not.
+  final bool tail;
+
+  static const double _radius = 10;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final h = size.height;
+    final w = size.width;
+    if (alignment == Alignment.topRight) {
+      if (tail) {
+        final path =
+            Path()
+              /// starting point
+              ..moveTo(_radius * 2, 0)
+              /// top-left corner
+              ..quadraticBezierTo(0, 0, 0, _radius * 1.5)
+              /// left line
+              ..lineTo(0, h - _radius * 1.5)
+              /// bottom-left corner
+              ..quadraticBezierTo(0, h, _radius * 2, h)
+              /// bottom line
+              ..lineTo(w - _radius * 3, h)
+              /// bottom-right bubble curve
+              ..quadraticBezierTo(w - _radius * 1.5, h, w - _radius * 1.5, h - _radius * 0.6)
+              /// bottom-right tail curve 1
+              ..quadraticBezierTo(w - _radius * 1, h, w, h)
+              /// bottom-right tail curve 2
+              ..quadraticBezierTo(w - _radius * 0.8, h, w - _radius, h - _radius * 1.5)
+              /// right line
+              ..lineTo(w - _radius, _radius * 1.5)
+              /// top-right curve
+              ..quadraticBezierTo(w - _radius, 0, w - _radius * 3, 0);
+
+        canvas
+          ..clipPath(path)
+          ..drawRRect(
+            RRect.fromLTRBR(0, 0, w, h, Radius.zero),
+            Paint()
+              ..color = color
+              ..style = PaintingStyle.fill,
+          );
+      } else {
+        final path =
+            Path()
+              /// starting point
+              ..moveTo(_radius * 2, 0)
+              /// top-left corner
+              ..quadraticBezierTo(0, 0, 0, _radius * 1.5)
+              /// left line
+              ..lineTo(0, h - _radius * 1.5)
+              /// bottom-left corner
+              ..quadraticBezierTo(0, h, _radius * 2, h)
+              /// bottom line
+              ..lineTo(w - _radius * 3, h)
+              /// bottom-right curve
+              ..quadraticBezierTo(w - _radius, h, w - _radius, h - _radius * 1.5)
+              /// right line
+              ..lineTo(w - _radius, _radius * 1.5)
+              /// top-right curve
+              ..quadraticBezierTo(w - _radius, 0, w - _radius * 3, 0);
+
+        canvas
+          ..clipPath(path)
+          ..drawRRect(
+            RRect.fromLTRBR(0, 0, w, h, Radius.zero),
+            Paint()
+              ..color = color
+              ..style = PaintingStyle.fill,
+          );
+      }
+    } else {
+      if (tail) {
+        final path =
+            Path()
+              /// starting point
+              ..moveTo(_radius * 3, 0)
+              /// top-left corner
+              ..quadraticBezierTo(_radius, 0, _radius, _radius * 1.5)
+              /// left line
+              ..lineTo(_radius, h - _radius * 1.5)
+              // bottom-right tail curve 1
+              ..quadraticBezierTo(_radius * .8, h, 0, h)
+              /// bottom-right tail curve 2
+              ..quadraticBezierTo(_radius * 1, h, _radius * 1.5, h - _radius * 0.6)
+              /// bottom-left bubble curve
+              ..quadraticBezierTo(_radius * 1.5, h, _radius * 3, h)
+              /// bottom line
+              ..lineTo(w - _radius * 2, h)
+              /// bottom-right curve
+              ..quadraticBezierTo(w, h, w, h - _radius * 1.5)
+              /// right line
+              ..lineTo(w, _radius * 1.5)
+              /// top-right curve
+              ..quadraticBezierTo(w, 0, w - _radius * 2, 0);
+        canvas
+          ..clipPath(path)
+          ..drawRRect(
+            RRect.fromLTRBR(0, 0, w, h, Radius.zero),
+            Paint()
+              ..color = color
+              ..style = PaintingStyle.fill,
+          );
+      } else {
+        final path =
+            Path()
+              /// starting point
+              ..moveTo(_radius * 3, 0)
+              /// top-left corner
+              ..quadraticBezierTo(_radius, 0, _radius, _radius * 1.5)
+              /// left line
+              ..lineTo(_radius, h - _radius * 1.5)
+              /// bottom-left curve
+              ..quadraticBezierTo(_radius, h, _radius * 3, h)
+              /// bottom line
+              ..lineTo(w - _radius * 2, h)
+              /// bottom-right curve
+              ..quadraticBezierTo(w, h, w, h - _radius * 1.5)
+              /// right line
+              ..lineTo(w, _radius * 1.5)
+              /// top-right curve
+              ..quadraticBezierTo(w, 0, w - _radius * 2, 0);
+
+        canvas
+          ..clipPath(path)
+          ..drawRRect(
+            RRect.fromLTRBR(0, 0, w, h, Radius.zero),
+            Paint()
+              ..color = color
+              ..style = PaintingStyle.fill,
+          );
+      }
+    }
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
+  }
+}
--- lib/widgets/card/post_card/show_user_brief_profile_dialog.dart
+++ lib/widgets/card/post_card/show_user_brief_profile_dialog.dart
@@ -14,6 +14,7 @@ import 'package:tsdm_client/routes/screen_paths.dart';
 import 'package:tsdm_client/shared/models/medal.dart';
 import 'package:tsdm_client/shared/models/models.dart';
 import 'package:tsdm_client/utils/html/html_muncher.dart';
+import 'package:tsdm_client/widgets/bubble.dart';
 import 'package:tsdm_client/widgets/cached_image/cached_image.dart';
 import 'package:tsdm_client/widgets/cached_image/cached_image_provider.dart';
 import 'package:tsdm_client/widgets/card/post_card/checkin.dart';
@@ -412,7 +413,7 @@ class _UserBriefProfileDialogState extends State<_UserBriefProfileDialog> {
                             ),
                             sizedBoxW8H8,
                             CustomPaint(
-                              painter: _SpecialChatBubbleThree(
+                              painter: BubblePainter(
                                 color: Theme.of(context).colorScheme.surfaceContainer,
                                 alignment: Alignment.topLeft,
                                 tail: true,
@@ -464,154 +465,5 @@ class _UserProfilePair extends StatelessWidget {
         Expanded(child: Text(value ?? '')),
       ],
     );
-  }
-}
-
-//custom painter use to create the shape of the chat bubble
-///
-/// [color],[alignment] and [tail] can be changed
-
-class _SpecialChatBubbleThree extends CustomPainter {
-  const _SpecialChatBubbleThree({required this.color, required this.alignment, required this.tail});
-
-  final Color color;
-  final Alignment alignment;
-  final bool tail;
-
-  static const double _radius = 10;
-
-  @override
-  void paint(Canvas canvas, Size size) {
-    final h = size.height;
-    final w = size.width;
-    if (alignment == Alignment.topRight) {
-      if (tail) {
-        final path =
-            Path()
-              /// starting point
-              ..moveTo(_radius * 2, 0)
-              /// top-left corner
-              ..quadraticBezierTo(0, 0, 0, _radius * 1.5)
-              /// left line
-              ..lineTo(0, h - _radius * 1.5)
-              /// bottom-left corner
-              ..quadraticBezierTo(0, h, _radius * 2, h)
-              /// bottom line
-              ..lineTo(w - _radius * 3, h)
-              /// bottom-right bubble curve
-              ..quadraticBezierTo(w - _radius * 1.5, h, w - _radius * 1.5, h - _radius * 0.6)
-              /// bottom-right tail curve 1
-              ..quadraticBezierTo(w - _radius * 1, h, w, h)
-              /// bottom-right tail curve 2
-              ..quadraticBezierTo(w - _radius * 0.8, h, w - _radius, h - _radius * 1.5)
-              /// right line
-              ..lineTo(w - _radius, _radius * 1.5)
-              /// top-right curve
-              ..quadraticBezierTo(w - _radius, 0, w - _radius * 3, 0);
-
-        canvas
-          ..clipPath(path)
-          ..drawRRect(
-            RRect.fromLTRBR(0, 0, w, h, Radius.zero),
-            Paint()
-              ..color = color
-              ..style = PaintingStyle.fill,
-          );
-      } else {
-        final path =
-            Path()
-              /// starting point
-              ..moveTo(_radius * 2, 0)
-              /// top-left corner
-              ..quadraticBezierTo(0, 0, 0, _radius * 1.5)
-              /// left line
-              ..lineTo(0, h - _radius * 1.5)
-              /// bottom-left corner
-              ..quadraticBezierTo(0, h, _radius * 2, h)
-              /// bottom line
-              ..lineTo(w - _radius * 3, h)
-              /// bottom-right curve
-              ..quadraticBezierTo(w - _radius, h, w - _radius, h - _radius * 1.5)
-              /// right line
-              ..lineTo(w - _radius, _radius * 1.5)
-              /// top-right curve
-              ..quadraticBezierTo(w - _radius, 0, w - _radius * 3, 0);
-
-        canvas
-          ..clipPath(path)
-          ..drawRRect(
-            RRect.fromLTRBR(0, 0, w, h, Radius.zero),
-            Paint()
-              ..color = color
-              ..style = PaintingStyle.fill,
-          );
-      }
-    } else {
-      if (tail) {
-        final path =
-            Path()
-              /// starting point
-              ..moveTo(_radius * 3, 0)
-              /// top-left corner
-              ..quadraticBezierTo(_radius, 0, _radius, _radius * 1.5)
-              /// left line
-              ..lineTo(_radius, h - _radius * 1.5)
-              // bottom-right tail curve 1
-              ..quadraticBezierTo(_radius * .8, h, 0, h)
-              /// bottom-right tail curve 2
-              ..quadraticBezierTo(_radius * 1, h, _radius * 1.5, h - _radius * 0.6)
-              /// bottom-left bubble curve
-              ..quadraticBezierTo(_radius * 1.5, h, _radius * 3, h)
-              /// bottom line
-              ..lineTo(w - _radius * 2, h)
-              /// bottom-right curve
-              ..quadraticBezierTo(w, h, w, h - _radius * 1.5)
-              /// right line
-              ..lineTo(w, _radius * 1.5)
-              /// top-right curve
-              ..quadraticBezierTo(w, 0, w - _radius * 2, 0);
-        canvas
-          ..clipPath(path)
-          ..drawRRect(
-            RRect.fromLTRBR(0, 0, w, h, Radius.zero),
-            Paint()
-              ..color = color
-              ..style = PaintingStyle.fill,
-          );
-      } else {
-        final path =
-            Path()
-              /// starting point
-              ..moveTo(_radius * 3, 0)
-              /// top-left corner
-              ..quadraticBezierTo(_radius, 0, _radius, _radius * 1.5)
-              /// left line
-              ..lineTo(_radius, h - _radius * 1.5)
-              /// bottom-left curve
-              ..quadraticBezierTo(_radius, h, _radius * 3, h)
-              /// bottom line
-              ..lineTo(w - _radius * 2, h)
-              /// bottom-right curve
-              ..quadraticBezierTo(w, h, w, h - _radius * 1.5)
-              /// right line
-              ..lineTo(w, _radius * 1.5)
-              /// top-right curve
-              ..quadraticBezierTo(w, 0, w - _radius * 2, 0);
-
-        canvas
-          ..clipPath(path)
-          ..drawRRect(
-            RRect.fromLTRBR(0, 0, w, h, Radius.zero),
-            Paint()
-              ..color = color
-              ..style = PaintingStyle.fill,
-          );
-      }
-    }
-  }
-
-  @override
-  bool shouldRepaint(CustomPainter oldDelegate) {
-    return true;
   }
 }
