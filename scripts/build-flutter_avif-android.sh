f50d3769e521049bfdc7d2e51bb5021d9c9abb9f
realth000 <a598919335@gmail.com>
2025-04-08 01:43:42 +0800

    build: test pinning NDK r28 for workflow

--- scripts/build-flutter_avif-android.sh
+++ scripts/build-flutter_avif-android.sh
@@ -2,6 +2,26 @@
 
 set -ex
 
+export ANDROID_HOME='/usr/local/lib/android/sdk'
+export ANDROID_NDK='/usr/local/lib/android/sdk/ndk/28.0.13004108'
+export ANDROID_NDK_HOME='/usr/local/lib/android/sdk/ndk/28.0.13004108'
+export ANDROID_NDK_LATEST_HOME='/usr/local/lib/android/sdk/ndk/28.0.13004108'
+export ANDROID_NDK_ROOT='/usr/local/lib/android/sdk/ndk/28.0.13004108'
+export ANDROID_SDK_ROOT='/usr/local/lib/android/sdk'
+
+if [ -d '/usr/local/lib/android/sdk/ndk/28.0.13004108' ];then
+  echo "NDK r28 exists"
+else
+  echo "NDK r28 NOT EXISTS"
+  exit 1
+fi
+
+if [ -d '/usr/local/lib/android/sdk/ndk/27.2.12479018' ];then
+  echo "NDK r27c exists"
+  echo "delete r27c"
+  mv '/usr/local/lib/android/sdk/ndk/27.2.12479018' '/usr/local/lib/android/sdk/ndk/27.2.12479018.bak'
+fi
+
 AVIF_ROOT="packages/flutter_avif"
 AVIF_ANDROID_JNILIBS_DIR="${AVIF_ROOT}/flutter_avif_android/android/src/main/jniLibs"
 
