10aa01e91f1272eac94c7177a65b3788e3625092
realth000 <a598919335@gmail.com>
2025-06-20 21:55:35 +0800

    fix(widgets): constrains maximum size of fallback picture
    Fixes #302

--- lib/widgets/fallback_picture.dart
+++ lib/widgets/fallback_picture.dart
@@ -19,12 +19,17 @@ class FallbackPicture extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Image.asset(
-      assetsLogoPngPath,
-      color: Theme.of(context).colorScheme.outlineVariant,
-      fit: fit,
-      width: width,
-      height: height,
+    return ConstrainedBox(
+      // Set a maximum size to make the loading process and error state fit with other contents' size, also avoid
+      // never-intended infinite size value passed by caller.
+      constraints: const BoxConstraints(maxWidth: 64, maxHeight: 64),
+      child: Image.asset(
+        assetsLogoPngPath,
+        color: Theme.of(context).colorScheme.outlineVariant,
+        fit: fit,
+        width: width,
+        height: height,
+      ),
     );
   }
 }
