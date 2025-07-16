1dd4ef70ce7faaab0ae43e3be98de4ba31cf00fd
realth000 <a598919335@gmail.com>
2025-07-17 06:02:22 +0800

    fix(chat): fix failed to redirect to user space page
    Use uid as parameter, if present.
    
    Fixes #174

--- lib/features/chat/models/models.dart
+++ lib/features/chat/models/models.dart
@@ -1,12 +1,10 @@
 import 'package:dart_mappable/dart_mappable.dart';
+import 'package:tsdm_client/extensions/string.dart';
 import 'package:tsdm_client/extensions/universal_html.dart';
 import 'package:tsdm_client/instance.dart';
 import 'package:universal_html/html.dart' as uh;
 
 part 'chat_history_send_target.dart';
-
 part 'chat_message.dart';
-
 part 'chat_send_target.dart';
-
 part 'models.mapper.dart';
